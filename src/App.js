7ddb997474f935e0b20af8d195bc9188fe3d6d35
Alejo Libreros <alrosdesign@gmail.com>
2021-07-09 02:00:51 -0500

    esto es una prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Category from "./Category";
+import Products from "./Products";
+import Login from './Login';
+import PrivateRoute from "./PrivateRoute";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Admin = () => (
+  <div>
+    <h2>Welcome admin!</h2>
+  </div>
+);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin area</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/category"><Category /></Route>
+        <Route path="/products"><Products /></Route>
+        <Route path="/login"><Login /></Route>
+        <PrivateRoute path="/admin" component={Admin} />
+      </Switch>
     </div>
   );
-}
-
-export default App;
+};
