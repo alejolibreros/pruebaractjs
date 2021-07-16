57e476408fbe3ebfad98e6ad860324b9f1659486
Alejo Libreros <alrosdesign@gmail.com>
2021-07-15 19:08:51 -0500

    Datos contacto, cambio tamaño fuente, botón login en menú

--- src/App.js
+++ src/App.js
@@ -66,8 +66,36 @@ const Home = () => (
           <h3 style={{ color: 'white' }}>Contáctanos</h3>  </center>
     &nbsp;
     &nbsp;
+    <div className="contenedor">
+      <div className="columna31">
+      &nbsp;
+      &nbsp;
+      <center>
+          <h3 style={{ color: 'white' }}>Datos de contacto
+            </h3>  
+      &nbsp;
+      &nbsp; 
+      <p style={{ color: 'white' }}>Celular: 318 596 59 62
+            </p>  
+      &nbsp;
+      &nbsp;   
+      <p style={{ color: 'white' }}>Dirección: Calle 2 #3-56
+            </p>  
+      &nbsp;
+      &nbsp;  
+      <p style={{ color: 'white' }}>Email: info@adoptauncallejerito.com
+            </p>  
+      &nbsp;
+      &nbsp;                  
+        </center>
+      &nbsp;
+      &nbsp;  
+      </div>  
+      <div className="columna32">
+
           <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31806.554586731294!2d-75.72667145591922!3d4.801054053184397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e388748eb56c1fd%3A0x95b39410f9f1dfbc!2sPereira%2C%20Risaralda!5e0!3m2!1ses-419!2sco!4v1626285175835!5m2!1ses-419!2sco"
+              title="Mapa"
               width="100%"
               height="500"
               frameBorder="0"
@@ -75,8 +103,10 @@ const Home = () => (
               allowFullScreen=""
               aria-hidden="false"
               tabIndex="0"
+              
             />
-
+      </div>
+    </div>
   </div>
 );
 
@@ -152,6 +182,10 @@ export default function App() {
         <ul className="nav navbar-nav">
           <li> 
             <Link to="/"> <div className="logo"></div></Link>          
+          </li>
+          <li>
+            
+            <Link to="/admin">Login</Link>
           </li>         
         </ul>
       </nav>
--- src/Login.js
+++ src/Login.js
@@ -20,6 +20,7 @@ export default function Login() {
 
   return (
     <div>
+
       <center>
       <label>
       Nombre:
