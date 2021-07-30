a1655c9aa9cef7c87417fff2925e69ffa7820163
aalvaroalvarez <alvanalar@gmail.com>
2021-07-30 12:31:33 -0500

    Agregar nombre a las funciones en auth y error reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -6,7 +6,7 @@ const initialState = {
   user: {},
   loading: false,
 };
-export default function (state = initialState, action) {
+export default function fun2 (state = initialState, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
