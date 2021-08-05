416bc7a0e3c58748233aa9904322280e24bf169c
aalvaroalvarez <alvanalar@gmail.com>
2021-08-05 12:58:13 -0500

    Vista registrar usuario, modificación expiración token y msj de validación

--- backend/validation/register.js
+++ backend/validation/register.js
@@ -12,26 +12,26 @@ module.exports = function validateRegisterInput(data) {
 
   // Comprobación del nombre
   if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
+    errors.name = "Campo nombre es obligatorio";
   }
   // Verificación de correo electrónico
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
+    errors.email = "Campo del correo electrónico es obligatorio";
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+    errors.email = "Correo electrónico inválido";
   }
   // Verificación de contraseñas
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
+    errors.password = "Campo contraseña es obligatorio";
   }
   if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm password field is required";
+    errors.password2 = "Campo confirmar contraseña es obligatorio";
   }
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+    errors.password = "La contraseña debe tener al menos 6 caracteres";
   }
   if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
+    errors.password2 = "Las contraseñas deben coincidir";
   }
   
   return {
